1b48c7bdf651b57b4f07dab8197ab71c051dcffc
jgvms-iscteiul <jgvms@iscte-iul.pt>
2020-05-05 20:28:55 +0100

    Alerta de temperatura finalizado
    -Manda alerta quando está no intervalo de Limite - 10
    -Manda alerta que ultrapassou o limite ao fim de 10 medições
    -Enquanto está acima do limite manda alerta ao fim de 30 mediçoes
    (poderá ser parameterizável ?)
    -Ao fim de 5 medições abaixo do limite, se tiver estado acima do limite
    anteriormente manda alerta que voltou ao normal.

--- SID_G03/src/main/java/inserirNoSQL/Alerta.java
+++ SID_G03/src/main/java/inserirNoSQL/Alerta.java
@@ -15,9 +15,8 @@ public class Alerta {
 	private int upCounter;
 	private int upCounterLim;
 	private int downCounter;
-	double medicaoAnterior;
-	Timer timer = new Timer();
-	boolean acimaLimite = false;
+	private int nMedicoes;
+	private boolean acimaLim=false;
 
 	public Alerta(Statement myStatement) {
 		this.myStatement=myStatement;
@@ -42,7 +41,7 @@ public class Alerta {
 	}
 	
 	public void enviarAlerta(String dataHora) throws SQLException {
-		if (tmpAtual >= tmpLimite -10)
+		if (tmpAtual >= tmpLimite -10 && tmpAtual<=tmpLimite)
 			upCounter++;
 		else
 			upCounter = 0;
@@ -52,24 +51,32 @@ public class Alerta {
 					+ tmpLimite + ",'O sistema detetou que "
 					+ "houve uma subida da temperatura',true,'Risco de ultrapassar o limite!')");
 		}
-		if(tmpAtual>tmpLimite)
+		if(tmpAtual>tmpLimite && !acimaLim)
 			upCounterLim++;
 		else upCounterLim=0;
 		if (upCounterLim == 10) {
-			upCounter = 0;
+			upCounterLim = 0;
 			myStatement.executeUpdate("insert into alerta" + " values(0" + "," + dataHora + ",'tmp'," + tmpAtual + ","
 					+ tmpLimite + ",'O sistema detetou que a temperatura está acima do limite',true,'')");
+			acimaLim=true;
 		}
-		if(tmpAtual <=medicaoAnterior-0.5 && tmpAtual>tmpLimite) {
+		if(acimaLim && tmpAtual>tmpLimite)
+			nMedicoes++;
+		if(nMedicoes==30) {
+			myStatement.executeUpdate("insert into alerta" + " values(0" + "," + dataHora + ",'tmp'," + tmpAtual + ","
+					+ tmpLimite + ",'O sistema detetou que a temperatura continua acima do limite',true,'')");
+			nMedicoes=0;
+		}
+		if(tmpAtual <tmpLimite && acimaLim) {
 			downCounter++;
 		}else downCounter=0;
-		if(downCounter == 10) {
+		if(downCounter == 5) {
 			downCounter=0;
 			myStatement.executeUpdate("insert into alerta" + " values(0" + "," + dataHora + ",'tmp'," + tmpAtual + ","
-					+ tmpLimite + ",'O sistema detetou que a temperatura está a diminuir após "
-							+ "ter ultrapassado o limite',true,'')");
+					+ tmpLimite + ",'O sistema detetou que a temperatura voltou a estar abaixo do limite' "
+							+ ",true,'')");
+			acimaLim=false;
 		}
-		medicaoAnterior = tmpAtual;
 	}
 	
 
